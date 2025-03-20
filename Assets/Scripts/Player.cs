6f0de75948c7f15bc6365817757c37eeb5cb4516
dragonborn456 <63712441+dragonborn456@users.noreply.github.com>
2025-03-20 10:14:08 +0700

    Merged Pull Request #5: 20250320-1: Clear Counter Selected Visual logic.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,9 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public static Player Instance { get; private set; }
+
+
+
+    public event EventHandler<OnSelectedCounterChangedEventArgs> OnSelectedCounterChanged;
+    public class OnSelectedCounterChangedEventArgs : EventArgs
+	{
+        public ClearCounter selectedCounter;
+	}
 
     // VARIABLES
     [SerializeField] private float moveSpeed = 7f;
@@ -12,7 +22,16 @@ public class Player : MonoBehaviour
 
     private bool isWalking;
     private Vector3 lastInteractDir;
+    private ClearCounter selectedCounter;
 
+	private void Awake()
+	{
+		if(Instance != null)
+		{
+            Debug.LogError("More than one Player instance have been created!");
+		}
+        Instance = this;
+	}
 	private void Start()
 	{
 		gameInput.OnInteractAction += GameInput_OnInteractAction;
@@ -20,30 +39,17 @@ public class Player : MonoBehaviour
 
 	private void GameInput_OnInteractAction(object sender, System.EventArgs e)
 	{
-        float interactDistance = 2f;
-
-        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
-        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-
-        // Save latest move direction when stopped.
-        if (moveDir != Vector3.zero) lastInteractDir = moveDir;
-
-        // Check object interaction
-        if (Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDistance, countersLayerMasks))
-        {
-            if (raycastHit.transform.TryGetComponent(out ClearCounter clearcounter))
-            {
-                // Clear counter available
-                clearcounter.Interact();
-            }
-        }
+        if(selectedCounter != null)
+		{
+            selectedCounter.Interact();
+		}
     }
 
 	// Update is called once per frame
 	private void Update()
     {
         HandleMovement();
-        HandleINteractions();
+        HandleInteractions();
     }
 
     public bool IsWalking()
@@ -52,7 +58,7 @@ public class Player : MonoBehaviour
 
     }
 
-    private void HandleINteractions()
+    private void HandleInteractions()
 	{
         float interactDistance = 2f;
 
@@ -68,8 +74,20 @@ public class Player : MonoBehaviour
             if(raycastHit.transform.TryGetComponent(out ClearCounter clearcounter))
             {
                 // Clear counter available
+                if(clearcounter != selectedCounter)
+				{
+                    SetSelectedCounter(clearcounter);
+				}
+			}
+			else
+			{
+                SetSelectedCounter(null);
 			}
 		}
+		else
+		{
+            SetSelectedCounter(null);
+        }
 	}
 
     private void HandleMovement()
@@ -131,5 +149,15 @@ public class Player : MonoBehaviour
         // Rotate the object based on the move direction
         float rotateSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
+    }
+
+    private void SetSelectedCounter(ClearCounter selectedCounter)
+	{
+        this.selectedCounter = selectedCounter;
+
+        OnSelectedCounterChanged?.Invoke(this, new OnSelectedCounterChangedEventArgs
+        {
+            selectedCounter = selectedCounter
+        });
     }
 }
